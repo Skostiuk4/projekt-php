8b02b5655d1fcfaf9580a40d0ea401bb0861b628
Skostiuk4 <Skostiuk4@gmail.com>
2019-09-16 16:05:27 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>project-php</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="logotip" ></div>
+	<img src="logo.png" alt="logo">
+</body>
+</html>
